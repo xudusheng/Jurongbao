72af7e3daa21915aceba8ec19c16af5d6233a29e
瑾宝小公举 <597132100@qq.com>
2015-10-11 14:37:09 +0800

    新建tabbarController

--- Jurongbao/JRBMainTabBarController.h
+++ Jurongbao/JRBMainTabBarController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  JRBMainTabBarController.h
 //  Jurongbao
 //
 //  Created by wangrongchao on 15/10/11.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface JRBMainTabBarController : UITabBarController
 
 @end
-
